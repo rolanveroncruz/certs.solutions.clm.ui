f63b469faf3be1c021c3c5d7211e9438f10a768b
rolanveroncruz <114793101+rolanveroncruz@users.noreply.github.com>
2026-02-05 13:34:34 +0800

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {App} from './app';
+import {LoginComponent} from './login-component/login-component';
+import {LandingPageComponent} from './landing-page-component/landing-page-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',  pathMatch: 'full', component:LandingPageComponent},
+  { path: 'login', pathMatch: 'full', component:LoginComponent },
+
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatChip, MatChipSet} from '@angular/material/chips';
+import {MatButton} from '@angular/material/button';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from '@angular/material/card';
+import {MatDivider, MatList, MatListItem} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbar, MatIcon, RouterLink, MatChipSet, MatChip, MatButton, MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardActions, MatList, MatListItem, MatDivider, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  readonly currentYear = new Date().getFullYear();
   protected title = 'certs.solutions.clm.ui';
 }
